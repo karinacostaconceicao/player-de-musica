8c69d031e20fd973fbeb5624a58bc3f5a86c9f7d
karin <Karina.cmc@outlook.com>
2022-04-05 14:19:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+let musicas = [
+    {titulo:'Hino do Palmeiras', artista: 'Melhor time', src:'musicas/HINO DO PALMEIRAS(MP3_320K).mp3', img:'img/palmeiras.jpg'},
+    {titulo:'Serial Killer', artista: 'Lana Del Ray', src:'musicas/Lana Del Rey - Serial Killer (Demo).mp3', img:'img/serialKiller.jpg'},
+    {titulo:'Here', artista: 'Alessia Cara', src:'musicas/Alessia Cara - Here (Lucian Remix).mp3', img:'img/here.jpg'}
+];
+
+// Instanciando uma variável p/ música 
+let musica = document.querySelector('audio');
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
+
+// Eventos
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
+musica.addEventListener('timeupdate', atualizarBarra);
+
+// Função anônima
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proximo').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2){
+        indexMusica = 0
+    }
+    renderizarMusica(indexMusica);
+});
+
+// Funções
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
+function tocarMusica(){
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
+
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
+}
+
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let  tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
+
+function segundosParaMinutos(segundos){
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (segundos < 10) {
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos + ':' + campoSegundos;
+}
+
